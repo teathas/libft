62b50ebb83a6fa3d3b66b03eebce57684f2ecdbd
Asma Berkasse <aberkass@e1r2p8.1337.ma>
2024-11-09 20:12:54 +0100

    updated

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: aberkass <aberkass@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 21:14:54 by aberkass          #+#    #+#             */
-/*   Updated: 2024/10/27 21:15:43 by aberkass         ###   ########.fr       */
+/*   Updated: 2024/11/09 19:49:54 by aberkass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,17 +14,19 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	int		i;
+	char	a;
 	char	*tmp;
 
 	i = 0;
 	tmp = NULL;
+	a = (char)c;
 	while (s[i] != '\0')
 	{
-		if (s[i] == (char)c)
+		if (s[i] == a)
 			tmp = (char *)&s[i];
 		i++;
 	}
-	if (c == '\0')
+	if (a == '\0')
 		return ((char *)&s[i]);
 	return (tmp);
 }
