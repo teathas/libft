c5973eede293e1ebd092773d1bc96053450de628
Asma Berkasse <aberkass@e1r2p8.1337.ma>
2024-11-09 14:56:26 +0100

    updated

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: aberkass <aberkass@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 20:56:59 by aberkass          #+#    #+#             */
-/*   Updated: 2024/11/09 14:39:34 by aberkass         ###   ########.fr       */
+/*   Updated: 2024/11/09 14:53:51 by aberkass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_isalnum(int c)
 {
-	if ((c >= 48 && c <= 57) || (c >= 65 && c > 90) || (c >= 97 && c <= 122))
+	if (ft_isalpha(c) || ft_isdigit(c))
 		return (1);
 	return (0);
 }
