172950ffdf443572a3a5f83e91bb560473ac2dfd
Asma Berkasse <aberkass@e1r2p8.1337.ma>
2024-11-09 19:03:43 +0100

    updated

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: aberkass <aberkass@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 21:13:51 by aberkass          #+#    #+#             */
-/*   Updated: 2024/11/09 18:55:26 by aberkass         ###   ########.fr       */
+/*   Updated: 2024/11/09 18:59:19 by aberkass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,20 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	int	i;
+	int		i;
+	char	ch;
 
+	ch = (char) c;
 	i = 0;
-	if (c == '\0')
-	{
-		return ((char *)&s[i]);
-	}
 	while (s[i] != '\0')
 	{
-		if (s[i] == (char)c)
+		if (s[i] == c)
 			return ((char *)&s[i]);
 		i++;
+	}
+	if (s[i] == ch)
+	{
+		return ((char *)&s[i]);
 	}
 	return (NULL);
 }
