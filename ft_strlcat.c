1915b90051087380f28ec30d27d4164734334252
Asma Berkasse <aberkass@e1r4p8.1337.ma>
2024-11-13 20:55:15 +0100

    modified

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,9 +6,10 @@
 /*   By: aberkass <aberkass@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 21:10:24 by aberkass          #+#    #+#             */
-/*   Updated: 2024/11/12 17:14:23 by aberkass         ###   ########.fr       */
+/*   Updated: 2024/11/13 13:49:12 by aberkass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
@@ -17,13 +18,15 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	src_l;
 	size_t	i;
 
+	if (!dst && size == 0)
+		return (NULL);
 	dst_l = ft_strlen(dst);
 	src_l = ft_strlen(src);
 	if (size == 0)
 		return (src_l);
-	i = 0;
 	if (dst_l >= size)
 		return (src_l + size);
+	i = 0;
 	while (src[i] && i < size - dst_l - 1)
 	{
 		dst[dst_l + i] = src[i];
